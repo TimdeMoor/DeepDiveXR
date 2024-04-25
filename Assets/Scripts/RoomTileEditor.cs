61534415a20d45fc1e4fbf7e0427cac153cae789
Tim de Moor <timdemoor122@gmail.com>
2024-04-25 09:31:54 +0200

    started on tile system

---
+++ Assets/Scripts/RoomTileEditor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(RoomTile))]
+public class RoomTileEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        RoomTile tile = (RoomTile)target;
+
+        EditorGUILayout.EnumPopup(WallType);
+    }
+}
